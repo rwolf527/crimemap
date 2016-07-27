3ed0000c3074db326848cf6210339314145a7787
Richard Wolf <rwolf@cisco.com>
2016-07-27 10:20:52 -0400

    Complete

--- crimemap.py
+++ crimemap.py
@@ -2,22 +2,40 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+import datetime
+import dateparser
+import string
+
 import dbconfig
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
 else:
     from dbhelper import DBHelper
 
+categories = ['mugging', 'break-in']
 
 app = Flask(__name__)
 DB = DBHelper()
 
 
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
+
+
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
 
 @app.route("/add", methods=["POST"])
@@ -42,10 +60,22 @@ def clear():
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
+    if category not in categories:
+        return home()
+
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+
     description = request.form.get("description")
+    description = sanitize_string(request.form.get("description"))
+
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
